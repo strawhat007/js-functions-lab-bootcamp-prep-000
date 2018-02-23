6f2b7a83a15b77b2fdf6aa23383a7354e6b5b7e2
Lakin Rakestraw <strawhat007123@gmail.com>
2018-02-23 22:12:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// write your code below!
+earfunction logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toUpperCase()
+}
+
+function sayHappyHolidays {
+  return "Happy Holidays!"
+}

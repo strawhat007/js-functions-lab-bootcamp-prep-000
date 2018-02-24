266d9a1774fafbd2aa4806dc26411ea24a2237d5
Lakin Rakestraw <strawhat007123@gmail.com>
2018-02-24 01:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(holiday)(name) {
+function happyHolidayTo(holiday){name} {
   return `Happy ${holiday}, ${name}!`
 }
